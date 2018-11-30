76182acf3e1f23ce2add5e1485a816665fe2453d
rsol7 <rsolm72@gmail.com>
2018-11-29 17:44:25 -0700

    LinkContainer wrapping around user routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import './App.css';
 
 class App extends Component {
@@ -17,8 +18,12 @@ class App extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <NavItem href="/signup">Signup</NavItem>
-              <NavItem href="/login">Login</NavItem>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
