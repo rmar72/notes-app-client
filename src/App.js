a42db79479cf958c9151801cec483a5a46434f2f
rsol7 <rsolm72@gmail.com>
2018-11-29 17:39:28 -0700

    Navbar signup & login additions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 import './App.css';
 
 class App extends Component {
@@ -13,7 +13,14 @@ class App extends Component {
             <Navbar.Brand>
               <Link to="/"> Scratch </Link>
             </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem href="/signup">Signup</NavItem>
+              <NavItem href="/login">Login</NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
