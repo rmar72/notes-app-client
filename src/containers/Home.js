28de747703133c45e4b1460407bc0e526a128239
rmar72 <rsolm72@gmail.com>
2019-06-26 02:34:10 -0600

    Setting up Home Container for displaaying a landing state or a user's notes list

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
+import { PageHeader, ListGroup } from "react-bootstrap";
 import "./Home.css";
 
 export default class Home extends Component {
-    render(){
-        return (
-            <div className="Home">
-                <div className="lander">
-                    <h1> Scratch </h1>
-                    <p> A simple note taking app </p>
-                </div>
-            </div>
-        );
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        isLoading: true,
+        notes: []
+      };
     }
-}
+  
+    renderNotesList(notes) {
+      return null;
+    }
+  
+    renderLandingState() {
+      return (
+        <div className="lander">
+          <h1>Scratch</h1>
+          <p>A simple note taking app</p>
+        </div>
+      );
+    }
+  
+    renderNotes() {
+      return (
+        <div className="notes">
+          <PageHeader>Your Notes</PageHeader>
+          <ListGroup>
+            {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+          </ListGroup>
+        </div>
+      );
+    }
+  
+    render() {
+      return (
+        <div className="Home">
+          {this.props.isAuthenticated ? this.renderNotes() : this.renderLandingState()}
+        </div>
+      );
+    }
+  }
