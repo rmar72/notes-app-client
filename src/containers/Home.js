cecdffb8e26e798e48469f7611f84c1c13f9f657
rmar72 <rsolm72@gmail.com>
2019-06-26 03:12:59 -0600

    making API.GET list call for a user's notes! oh yeahh!

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup } from "react-bootstrap";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./Home.css";
+
+import { API } from "aws-amplify";
 
 export default class Home extends Component {
     constructor(props) {
@@ -8,12 +11,54 @@ export default class Home extends Component {
   
       this.state = {
         isLoading: true,
-        notes: []
+        notes: [],
+        userId: ""
       };
+    }
+
+    async componentDidMount() {
+        if (!this.props.isAuthenticated) {
+          return;
+        }
+      
+        try {
+          const notes = await this.notes();
+          this.setState({ notes });
+        } catch (e) {
+          alert(e);
+        }
+      
+        this.setState({ isLoading: false });
+    }
+      
+    notes() {
+        return API.get("notes", "/notes");
     }
   
     renderNotesList(notes) {
-      return null;
+        return [{}].concat(notes).map(
+          (note, i) =>
+            i !== 0
+              ? <LinkContainer
+                  key={note.noteId}
+                  to={`/notes/${note.noteId}`}
+                >
+                    {/* cu algo, note.content.trim().split("\n")[0] renders the first line of each note */}
+                  <ListGroupItem header={note.content.trim().split("\n")[0]}>
+                    {"Created: " + new Date(note.createdAt).toLocaleString()}
+                  </ListGroupItem>
+                </LinkContainer>
+              : <LinkContainer
+                  key="new"
+                  to="/notes/new"
+                >
+                  <ListGroupItem>
+                    <h4>
+                      <b>{"\uFF0B"}</b> Create a new note
+                    </h4>
+                  </ListGroupItem>
+                </LinkContainer>
+        );
     }
   
     renderLandingState() {
