a785cfe085a95ce4ab6e856067a53c108bb3a8d4
rmar72 <rsolm72@gmail.com>
2019-06-27 00:43:42 -0600

    rewriting the container to remove previous failed clutter

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,57 +1,44 @@
-import React, { Component } from 'react';
-import { API, Storage } from 'aws-amplify';
+import React, { Component } from "react";
+import { API, Storage } from "aws-amplify";
 
 export default class Notes extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.file = null;
+    this.file = null;
 
-        this.state = {
-            note: null,
-            content: '',
-            attachmentURL: null
-        };
+    this.state = {
+      note: null,
+      content: "",
+      attachmentURL: null
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      let attachmentURL;
+      const note = await this.getNote();
+      const { content, attachment } = note;
+
+      if (attachment) {
+        attachmentURL = await Storage.vault.get(attachment);
+      }
+
+      this.setState({
+        note,
+        content,
+        attachmentURL
+      });
+    } catch (e) {
+      alert(e);
     }
+  }
 
-    async componentDidMount(){
-        try{
-            const notes = await this.notes();
-            console.log("list of notes", notes);
+  getNote() {
+    return API.get("notes", `/notes/${this.props.match.params.id}`);
+  }
 
-            // let attachmentURL;
-            // const note = await this.getNote();
-            // console.log("a note", note)
-            // const { content, attachment } = note;
-
-            // console.log(note)
-
-            // if(attachment){
-            //     attachmentURL = await Storage.vault.get(attachment);
-            // }
-
-            // this.setState({
-            //     note,
-            //     content,
-            //     attachmentURL
-            // });
-        }
-        catch(e){
-            console.log(e)
-            alert(e);
-        }
-    }
-
-    getNote(){
-        return API.get('notes', `/notes/${this.props.match.params.id}`);
-    }
-
-    // this method from the other lesson works here & its a .get too! wth
-    notes() {
-        return API.get("notes", "/notes");
-    }
-
-    render(){
-        return <div className="Notes"></div>
-    }
+  render() {
+    return <div className="Notes"></div>;
+  }
 }
